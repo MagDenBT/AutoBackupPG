0a5e10d7e7910663c3fdcb08c4ca4dbe27dc1aef
Deni <magdenbt@gmail.com>
2022-11-02 18:14:24 +0300

    Refactoring

--- PosgreSQLBackuper.py
+++ PosgreSQLBackuper.py
@@ -2,42 +2,95 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# Этот модуль требуется импортировать в начале каждого скрипта.
-from lib.common import bootstrap
-# Экспорт базового класса для сценария. Он отвечает за настройку логгирования,
-# базовую валидацию параметров, перехват исключений и корректное завершение
-# скрипта и т.д.
-from lib.common.base_scenario import BaseScenario
-# Экспорт класса ошибок.
-from lib.common.errors import SACError
-
 import os
 import subprocess
 from datetime import datetime
 import random
 import requests as requests
 
-# CLoud settings
-URL = 'https://cloud-api.yandex.net/v1/disk/resources'
-TOKEN = '1111'
-headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
-rootCloudPath = 'Postgresql backups'
-fullBpCloudPath = 'Full'
-incrBpCloudPath = 'Incremental'
+args = {}
 
-#Local machine settings
+def main():
 
-customPath = 'Accounting department'                                     #Catalog for logical structure
-tempPathToFullBackup = 'C:\\pgBackIncr'                                  #The path to the temporary directory for full backup
-pathToFullBackup = f'C:\\{customPath}\\Postgresql full backups'          #The path to the permanent  directory for full backup
-pathToIncrementalBackup = 'C:\\pg_log_archive'                           #The path to the WAL files.(Incremental backups)
-logPath = 'C:\\pgBackupLog'                                              #The path to the script logs
+    setParam()
 
-postgresqlIsntancePath = "C:\\Program Files\\PostgreSQL\\14.4-1.1C\\"    #The path to PostgreSQL
-backuper = postgresqlIsntancePath + "bin\\pg_basebackup.exe"
-postgresqlUsername = "postgres"
-postgresqlPassword = '1122'
+    message = ''
+    if not checkParams(message):
+        writeLog('backup-', False, message)
 
+    try:
+        createFullBackup()
+        writeLog('backup-', True, '')
+    except Exception as e:
+        writeLog('backup-', False, str(e))
+
+    try:
+        uploadOnYandexCloud()
+        writeLog('upload-', True, '')
+    except Exception as e:
+        writeLog('upload-', False, str(e))
+
+def checkParams(message):
+    result = True
+    for item in args.items():
+        if item[1] == '':
+            message += f'Parameter "{item[0]}" is not set + \n'
+            result = False
+
+    return result
+
+def setParam(newArgs=None):
+
+    global args
+
+    if not newArgs == None:
+        args = newArgs
+    else:
+        # Generating Required Parameters
+
+        # CLoud settings
+        args.update('TOKEN', '1111')
+
+        # Paths settings
+        """
+        The directory structure is as follows:
+        On the local machine - disk:\rootDir\customDir\fullBpDir and for WAL-files - the one you specify
+        In the cloud - /rootDir/customDir/fullBpDir and for WAL-files - /rootDir/customDir/"LastDirectoryIn(localPathToWALFiles)"
+        """
+        args.update('disk', 'C')
+        args.update('rootDir', 'Postgresql backups')
+        args.update('customDir', 'Accounting department')
+        args.update('fullBpDir', 'Full')
+        args.update('localPathToWALFiles', 'C:\\pg_log_archive')    # The path of the WAL files.(aka Incremental backups)
+
+        # PostgreSQL settings
+        args.update('postgresqlIsntancePath', "C:\\Program Files\\PostgreSQL\\14.4-1.1C\\")  # The path to PostgreSQL
+        args.update('postgresqlUsername', "postgres")
+        args.update('postgresqlPassword', '1122')
+
+    _addParams(args)
+
+
+def _addParams(args):
+    args.update('headers', {'Content-Type': 'application/json', 'Accept': 'application/json',
+                            'Authorization': f'OAuth {args["TOKEN"]}'})
+    args.update('pathToFullBackup',
+                f'{args["rootDir"]}:\\{args["customDir"]}\\{args["fullBpDir"]}')  # The path to the permanent  directory for full backup
+
+    incrBpDir = os.path.basename(args["localPathToWALFiles"])
+    args.update('incrBpDir', incrBpDir)
+
+    if args.get('backuper') == None:
+        args.update('backuper', args["postgresqlIsntancePath"] + 'bin\\pg_basebackup.exe')
+
+    if args.get('tempPath') == None:
+        args.update('tempPath', './temp')  # The path to the temporary directory for full backup
+
+    if args.get('logPath') == None:
+        args.update('logPath', './logs')  # The path to the script logs
+
+    if args.get('URL') == None:
+        args.update('URL', 'https://cloud-api.yandex.net/v1/disk/resources')
 
 def generateLabel(millisec=False):
     if millisec:
@@ -48,43 +101,40 @@ def generateLabel(millisec=False):
     # label = now.strftime(f"%Y_%m_%d____%H-%M-%S{ssss}") + '_' + str(random.randint(1, 100))
     return timeStamp + '_' + str(random.randint(1, 100))
 
-
 def fileOperationsFullBackup():
-    files = getFilesListOnDisk(tempPathToFullBackup)
-    if not os.path.exists(f'{pathToFullBackup}\\{label}'):
-        os.makedirs(f'{pathToFullBackup}\\{label}')
+    files = getFilesListOnDisk(args["tempPath"])
+    if not os.path.exists(f'{args["pathToFullBackup"]}\\{label}'):
+        os.makedirs(f'{args["pathToFullBackup"]}\\{label}')
 
     # move & rename
     for file in files:
-        os.replace(file, f'{pathToFullBackup}\\{label}\\{label}__{os.path.basename(file)}')
-
+        os.replace(file, f'{args["pathToFullBackup"]}\\{label}\\{label}__{os.path.basename(file)}')
 
 def clearTempDirFullBackup():
     # clear the directory of any files
-    if os.path.exists(tempPathToFullBackup):
-        for path in os.listdir(tempPathToFullBackup):
-            if os.path.exists(f'{tempPathToFullBackup}\\{path}'):
-                os.remove(f'{tempPathToFullBackup}\\{path}')
-
+    if os.path.exists(args["tempPath"]):
+        for path in os.listdir(args["tempPath"]):
+            if os.path.exists(f'{args["tempPath"]}\\{path}'):
+                os.remove(f'{args["tempPath"]}\\{path}')
 
 def createFullBackup():
     global label
     label = generateLabel()
 
     my_env = os.environ.copy()
-    my_env["PGPASSWORD"] = postgresqlPassword
+    my_env["PGPASSWORD"] = args["postgresqlPassword"]
 
     clearTempDirFullBackup()
 
     process = subprocess.run(
-        [backuper,
-         '-D', tempPathToFullBackup,
+        [args["backuper"],
+         '-D', args["tempPath"],
          '-X', 'fetch',
          '-F', 'tar',
          '--label', label,
          '--gzip',
          '--no-password',
-         '--username', postgresqlUsername,
+         '--username', args["postgresqlUsername"],
          ],
         stderr=subprocess.PIPE,
         env=my_env,
@@ -93,20 +143,17 @@ def createFullBackup():
     if textError == "":
         fileOperationsFullBackup()
         clearTempDirFullBackup()
-        writeLog('backup-', True)
     else:
         raise Exception(textError)
 
-
 def writeLog(filePref, success, text=''):
     result = "Success_" if success else 'FAIL_'
-    if not os.path.exists(logPath):
-        os.makedirs(logPath)
-    path = f'{logPath}\\{filePref}{result}{generateLabel(True)}.txt'
+    if not os.path.exists(args["logPath"]):
+        os.makedirs(args["ogPath"])
+    path = f'{args["logPath"]}\\{filePref}{result}{generateLabel(True)}.txt'
     file = open(path, "w")
     file.write(text)
     file.close()
-
 
 def getFilesListOnDisk(*args):
     filesList = []
@@ -122,7 +169,6 @@ def getFilesListOnDisk(*args):
 
     return filesList
 
-
 def getFilesToUpload(backups, rootDir):
     toUpload = []
     for backup in backups:
@@ -131,7 +177,7 @@ def getFilesToUpload(backups, rootDir):
         fileName = os.path.basename(backup)
 
         try:
-            res = requests.get(f'{URL}?path=/{rootCloudPath}/{rootDir}/{dirName}/{fileName}', headers=headers)
+            res = requests.get(f'{args["URL"]}?path=/{args["rootDir"]}/{rootDir}/{dirName}/{fileName}', headers=args["headers"])
             if not res.status_code == 200:
                 if res.status_code == 404:
                     toUpload.append(backup)
@@ -142,17 +188,15 @@ def getFilesToUpload(backups, rootDir):
 
     return toUpload
 
-
 def getAvailableMemory():
     try:
-        res = requests.get(f'https://cloud-api.yandex.net/v1/disk?fields=used_space%2Ctotal_space', headers=headers)
+        res = requests.get(f'https://cloud-api.yandex.net/v1/disk?fields=used_space%2Ctotal_space', headers=args["headers"])
         if not res.status_code == 200:
             raise Exception(f'Не удалось получить инфо о свободном месте в облаке. {res.text}')
         data = res.json()
         return data['total_space'] - data['used_space']
     except Exception as e:
         raise Exception(e)
-
 
 def prepareDirOnCloud(backups, rootDir):
     paths = []
@@ -163,19 +207,18 @@ def prepareDirOnCloud(backups, rootDir):
 
     paths = set(paths)
     for dirName in paths:
-        step =  '/' + customPath
+        step =  '/' + args["rootDir"]
         createDirOnCloud(step)
-        step += '/' + rootCloudPath
+        step += '/' + args["customDir"]
         createDirOnCloud(step)
         step += "/" + rootDir
         createDirOnCloud(step)
         step += "/" + dirName
         createDirOnCloud(step)
 
-
 def createDirOnCloud(path):
     try:
-        res = requests.put(f'{URL}?path={path}', headers=headers)
+        res = requests.put(f'{args["URL"]}?path={args["path"]}', headers=args["headers"])
 
         if not res.status_code == 201 and not res.status_code == 409:
             raise Exception(f'Не удалось создать каталог {path} в облаке. {res.text}')
@@ -183,13 +226,12 @@ def createDirOnCloud(path):
     except Exception as e:
         raise Exception(e)
 
-
 def uploadOnYandexCloud():
-    fullBackups = getFilesListOnDisk(pathToFullBackup)
-    incrBackups = getFilesListOnDisk(pathToIncrementalBackup)
+    fullBackups = getFilesListOnDisk(args["pathToFullBackup"])
+    incrBackups = getFilesListOnDisk(args["localPathToWALFiles"])
 
-    fullBackupsToUpload = getFilesToUpload(fullBackups, fullBpCloudPath)
-    incrBackupsToUpload = getFilesToUpload(incrBackups, incrBpCloudPath)
+    fullBackupsToUpload = getFilesToUpload(fullBackups, args["fullBpDir"])
+    incrBackupsToUpload = getFilesToUpload(incrBackups, args["incrBpDir"])
 
     uploadSize = 0
     for filePath in fullBackupsToUpload:
@@ -208,33 +250,31 @@ def uploadOnYandexCloud():
     if toClear > 0:
         raise Exception(f'Недостаточно места в облаке. Требуется еще {toClear / 1024 / 1024} мб')
 
-    prepareDirOnCloud(fullBackupsToUpload, fullBpCloudPath)
-    prepareDirOnCloud(incrBackupsToUpload, incrBpCloudPath)
+    prepareDirOnCloud(fullBackupsToUpload, args["fullBpDir"])
+    prepareDirOnCloud(incrBackupsToUpload, args["incrBpDir"])
 
     for backup in fullBackupsToUpload:
         dir = os.path.dirname(backup)
         dirName = os.path.basename(dir)
         filename = os.path.basename(backup)
-        upload_file(backup, f'/{rootCloudPath}/{fullBpCloudPath}/{dirName}/{filename}')
+        upload_file(backup, f'/{args["rootDir"]}/{args["fullBpDir"]}/{dirName}/{filename}')
 
     for backup in incrBackupsToUpload:
         dir = os.path.dirname(backup)
         dirName = os.path.basename(dir)
         filename = os.path.basename(backup)
-        upload_file(backup, f'/{rootCloudPath}/{incrBpCloudPath}/{dirName}/{filename}')
-
+        upload_file(backup, f'/{args["rootDir"]}/{args["incrBpDir"]}/{dirName}/{filename}')
 
 def create_folder(path):
     """Создание папки. \n path: Путь к создаваемой папке."""
-    requests.put(f'{URL}?path={path}', headers=headers)
-
+    requests.put(f'{args["URL"]}?path={path}', headers=args["headers"])
 
 def upload_file(loadfile, savefile, replace=False):
     """Загрузка файла.
     savefile: Путь к файлу на Диске
     loadfile: Путь к загружаемому файлу
     replace: true or false Замена файла на Диске"""
-    res = requests.get(f'{URL}/upload?path={savefile}&overwrite={replace}', headers=headers).json()
+    res = requests.get(f'{args["URL"]}/upload?path={savefile}&overwrite={replace}', headers=args["headers"]).json()
     with open(loadfile, 'rb') as f:
         try:
             res = requests.put(res['href'], files={'file': f})
@@ -243,24 +283,6 @@ def upload_file(loadfile, savefile, replace=False):
         except Exception as e:
             raise Exception(e)
 
-    ## Метод, реализующий основную логику. Он запускается в случае, если
-    # test-mode == False.
-    def _real(self):
-
-        try:
-            createFullBackup()
-            writeLog('backup-', True, '')
-        except Exception as e:
-            writeLog('backup-', False, str(e))
-
-        try:
-            uploadOnYandexCloud()
-            writeLog('upload-', True, '')
-        except Exception as e:
-            writeLog('upload-', False, str(e))
-
-
-# Позволяет запускать сценарий, если данный файл был запущен напрямую.
-if __name__ == "__main__":
-    PGBackupScenario.main()
-
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
