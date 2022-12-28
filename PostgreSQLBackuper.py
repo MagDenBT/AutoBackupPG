417552f4f2375b6f32df82a86dec89d62ec5fd08
Deni <magdenbt@gmail.com>
2022-12-29 00:27:07 +0300

    BugFix - The cleaner did not process backups without the manifest file

--- PostgreSQLBackuper.py
+++ PostgreSQLBackuper.py
@@ -8,6 +8,7 @@ import os
 import pathlib
 import shutil
 import subprocess
+import tarfile
 import traceback
 import datetime
 import random
@@ -21,14 +22,34 @@ import tzlocal
 class Func:
 
     @staticmethod
-    def get_objects_list_on_disk(path, mask=None, only_files=True):
+    def get_objects_list_on_disk(path, mask=None, second_mask=None, only_files=True):
         objects_list = []
         for root, dirs, files in os.walk(path):
+            total_files = len(files)
+            i = 0
+            temp = []
             for filename in files:
+                i += 1
                 if mask is not None:
-                    if mask not in filename:
-                        continue
-                objects_list.append(os.path.join(root, filename))
+                    if mask in filename:
+                        objects_list.append(os.path.join(root, filename))
+                        i = total_files + 1
+                    else:
+                        if second_mask is not None:
+                            if i == total_files:
+                                if len(temp) > 0:
+                                    for val in temp:
+                                        if second_mask in val:
+                                            objects_list.append(os.path.join(root, val))
+                                elif second_mask in filename:
+                                    objects_list.append(os.path.join(root, filename))
+                            else:
+                                temp.append(filename)
+                                continue
+                        else:
+                            continue
+                else:
+                    objects_list.append(os.path.join(root, filename))
 
             if not only_files:
                 for _dir in dirs:
@@ -876,28 +897,266 @@ class AWS_Connector:
 
 class Cleaner:
 
+    whois_timezone_info = {}
     args = None
 
     def __init__(self, args):
         self.args = args
+        self.set_whois_timezone_info()
 
+    def set_whois_timezone_info(self):
+
+        self.whois_timezone_info = {
+            "A": 1 * 3600,
+            "ACDT": 10.5 * 3600,
+            "ACST": 9.5 * 3600,
+            "ACT": -5 * 3600,
+            "ACWST": 8.75 * 3600,
+            "ADT": 4 * 3600,
+            "AEDT": 11 * 3600,
+            "AEST": 10 * 3600,
+            "AET": 10 * 3600,
+            "AFT": 4.5 * 3600,
+            "AKDT": -8 * 3600,
+            "AKST": -9 * 3600,
+            "ALMT": 6 * 3600,
+            "AMST": -3 * 3600,
+            "AMT": -4 * 3600,
+            "ANAST": 12 * 3600,
+            "ANAT": 12 * 3600,
+            "AQTT": 5 * 3600,
+            "ART": -3 * 3600,
+            "AST": 3 * 3600,
+            "AT": -4 * 3600,
+            "AWDT": 9 * 3600,
+            "AWST": 8 * 3600,
+            "AZOST": 0 * 3600,
+            "AZOT": -1 * 3600,
+            "AZST": 5 * 3600,
+            "AZT": 4 * 3600,
+            "AoE": -12 * 3600,
+            "B": 2 * 3600,
+            "BNT": 8 * 3600,
+            "BOT": -4 * 3600,
+            "BRST": -2 * 3600,
+            "BRT": -3 * 3600,
+            "BST": 6 * 3600,
+            "BTT": 6 * 3600,
+            "C": 3 * 3600,
+            "CAST": 8 * 3600,
+            "CAT": 2 * 3600,
+            "CCT": 6.5 * 3600,
+            "CDT": -5 * 3600,
+            "CEST": 2 * 3600,
+            "CET": 1 * 3600,
+            "CHADT": 13.75 * 3600,
+            "CHAST": 12.75 * 3600,
+            "CHOST": 9 * 3600,
+            "CHOT": 8 * 3600,
+            "CHUT": 10 * 3600,
+            "CIDST": -4 * 3600,
+            "CIST": -5 * 3600,
+            "CKT": -10 * 3600,
+            "CLST": -3 * 3600,
+            "CLT": -4 * 3600,
+            "COT": -5 * 3600,
+            "CST": -6 * 3600,
+            "CT": -6 * 3600,
+            "CVT": -1 * 3600,
+            "CXT": 7 * 3600,
+            "ChST": 10 * 3600,
+            "D": 4 * 3600,
+            "DAVT": 7 * 3600,
+            "DDUT": 10 * 3600,
+            "E": 5 * 3600,
+            "EASST": -5 * 3600,
+            "EAST": -6 * 3600,
+            "EAT": 3 * 3600,
+            "ECT": -5 * 3600,
+            "EDT": -4 * 3600,
+            "EEST": 3 * 3600,
+            "EET": 2 * 3600,
+            "EGST": 0 * 3600,
+            "EGT": -1 * 3600,
+            "EST": -5 * 3600,
+            "ET": -5 * 3600,
+            "F": 6 * 3600,
+            "FET": 3 * 3600,
+            "FJST": 13 * 3600,
+            "FJT": 12 * 3600,
+            "FKST": -3 * 3600,
+            "FKT": -4 * 3600,
+            "FNT": -2 * 3600,
+            "G": 7 * 3600,
+            "GALT": -6 * 3600,
+            "GAMT": -9 * 3600,
+            "GET": 4 * 3600,
+            "GFT": -3 * 3600,
+            "GILT": 12 * 3600,
+            "GMT": 0 * 3600,
+            "GST": 4 * 3600,
+            "GYT": -4 * 3600,
+            "H": 8 * 3600,
+            "HDT": -9 * 3600,
+            "HKT": 8 * 3600,
+            "HOVST": 8 * 3600,
+            "HOVT": 7 * 3600,
+            "HST": -10 * 3600,
+            "I": 9 * 3600,
+            "ICT": 7 * 3600,
+            "IDT": 3 * 3600,
+            "IOT": 6 * 3600,
+            "IRDT": 4.5 * 3600,
+            "IRKST": 9 * 3600,
+            "IRKT": 8 * 3600,
+            "IRST": 3.5 * 3600,
+            "IST": 5.5 * 3600,
+            "JST": 9 * 3600,
+            "K": 10 * 3600,
+            "KGT": 6 * 3600,
+            "KOST": 11 * 3600,
+            "KRAST": 8 * 3600,
+            "KRAT": 7 * 3600,
+            "KST": 9 * 3600,
+            "KUYT": 4 * 3600,
+            "L": 11 * 3600,
+            "LHDT": 11 * 3600,
+            "LHST": 10.5 * 3600,
+            "LINT": 14 * 3600,
+            "M": 12 * 3600,
+            "MAGST": 12 * 3600,
+            "MAGT": 11 * 3600,
+            "MART": 9.5 * 3600,
+            "MAWT": 5 * 3600,
+            "MDT": -6 * 3600,
+            "MHT": 12 * 3600,
+            "MMT": 6.5 * 3600,
+            "MSD": 4 * 3600,
+            "MSK": 3 * 3600,
+            "MST": -7 * 3600,
+            "MT": -7 * 3600,
+            "MUT": 4 * 3600,
+            "MVT": 5 * 3600,
+            "MYT": 8 * 3600,
+            "N": -1 * 3600,
+            "NCT": 11 * 3600,
+            "NDT": 2.5 * 3600,
+            "NFT": 11 * 3600,
+            "NOVST": 7 * 3600,
+            "NOVT": 7 * 3600,
+            "NPT": 5.5 * 3600,
+            "NRT": 12 * 3600,
+            "NST": 3.5 * 3600,
+            "NUT": -11 * 3600,
+            "NZDT": 13 * 3600,
+            "NZST": 12 * 3600,
+            "O": -2 * 3600,
+            "OMSST": 7 * 3600,
+            "OMST": 6 * 3600,
+            "ORAT": 5 * 3600,
+            "P": -3 * 3600,
+            "PDT": -7 * 3600,
+            "PET": -5 * 3600,
+            "PETST": 12 * 3600,
+            "PETT": 12 * 3600,
+            "PGT": 10 * 3600,
+            "PHOT": 13 * 3600,
+            "PHT": 8 * 3600,
+            "PKT": 5 * 3600,
+            "PMDT": -2 * 3600,
+            "PMST": -3 * 3600,
+            "PONT": 11 * 3600,
+            "PST": -8 * 3600,
+            "PT": -8 * 3600,
+            "PWT": 9 * 3600,
+            "PYST": -3 * 3600,
+            "PYT": -4 * 3600,
+            "Q": -4 * 3600,
+            "QYZT": 6 * 3600,
+            "R": -5 * 3600,
+            "RET": 4 * 3600,
+            "ROTT": -3 * 3600,
+            "S": -6 * 3600,
+            "SAKT": 11 * 3600,
+            "SAMT": 4 * 3600,
+            "SAST": 2 * 3600,
+            "SBT": 11 * 3600,
+            "SCT": 4 * 3600,
+            "SGT": 8 * 3600,
+            "SRET": 11 * 3600,
+            "SRT": -3 * 3600,
+            "SST": -11 * 3600,
+            "SYOT": 3 * 3600,
+            "T": -7 * 3600,
+            "TAHT": -10 * 3600,
+            "TFT": 5 * 3600,
+            "TJT": 5 * 3600,
+            "TKT": 13 * 3600,
+            "TLT": 9 * 3600,
+            "TMT": 5 * 3600,
+            "TOST": 14 * 3600,
+            "TOT": 13 * 3600,
+            "TRT": 3 * 3600,
+            "TVT": 12 * 3600,
+            "U": -8 * 3600,
+            "ULAST": 9 * 3600,
+            "ULAT": 8 * 3600,
+            "UTC": 0 * 3600,
+            "UYST": -2 * 3600,
+            "UYT": -3 * 3600,
+            "UZT": 5 * 3600,
+            "V": -9 * 3600,
+            "VET": -4 * 3600,
+            "VLAST": 11 * 3600,
+            "VLAT": 10 * 3600,
+            "VOST": 6 * 3600,
+            "VUT": 11 * 3600,
+            "W": -10 * 3600,
+            "WAKT": 12 * 3600,
+            "WARST": -3 * 3600,
+            "WAST": 2 * 3600,
+            "WAT": 1 * 3600,
+            "WEST": 1 * 3600,
+            "WET": 0 * 3600,
+            "WFT": 12 * 3600,
+            "WGST": -2 * 3600,
+            "WGT": -3 * 3600,
+            "WIB": 7 * 3600,
+            "WIT": 9 * 3600,
+            "WITA": 8 * 3600,
+            "WST": 14 * 3600,
+            "WT": 0 * 3600,
+            "X": -11 * 3600,
+            "Y": -12 * 3600,
+            "YAKST": 10 * 3600,
+            "YAKT": 9 * 3600,
+            "YAPT": 10 * 3600,
+            "YEKST": 6 * 3600,
+            "YEKT": 5 * 3600,
+            "Z": 0 * 3600,
+        }
     def _clean_local(self, expire_date):
         full_bck = self.__full_bck_to_remove(expire_date)
         for obj in full_bck:
             os.remove(obj)
 
         mask = '__backup_manifest'
-        full_bck = Func.get_objects_list_on_disk(self.args.path_to_full_backup_local(), mask, only_files=True)
+        second_mask = '__base.'
+        full_bck = Func.get_objects_list_on_disk(self.args.path_to_full_backup_local(), mask=mask, second_mask=second_mask, only_files=True)
 
         oldest_date = datetime.datetime.now(tzlocal.get_localzone())
         oldest_label = None
         for file in full_bck:
             file_name = os.path.basename(file)
-            date_str = self.__read_create_date(file)
-            bck_date = parser.parse(date_str)
-            if oldest_date >= bck_date:
+            bck_date = self.__read_create_date(file)
+            if bck_date is not None and oldest_date >= bck_date:
                 oldest_date = bck_date
-                oldest_label = file_name.replace(mask, '')
+                if mask in file_name:
+                    current_mask = mask
+                else:
+                    current_mask = second_mask
+
+                oldest_label = file_name.replace(current_mask, '')
 
         if oldest_label is not None:
             inc_bck = self.__inc_bck_to_remove(self.args.local_path_to_wal_files(), oldest_label)
@@ -959,25 +1218,74 @@ class Cleaner:
 
     def __full_bck_to_remove(self, exprire_date: datetime):
         mask = '__backup_manifest'
-        full_bck = Func.get_objects_list_on_disk(self.args.path_to_full_backup_local(), mask=mask,  only_files=True)
+        second_mask = '__base.'
+        full_bck = Func.get_objects_list_on_disk(self.args.path_to_full_backup_local(), mask=mask, second_mask=second_mask, only_files=True)
         result = []
 
         for file in full_bck:
             file_name = os.path.basename(file)
-            date_str = self.__read_create_date(file)
-            bck_date = parser.parse(date_str)
-            if bck_date <= exprire_date:
+            bck_date = self.__read_create_date(file)
+            if bck_date is not None and bck_date <= exprire_date:
+                if mask in file_name:
+                    current_mask = mask
+                else:
+                    current_mask = second_mask
+
                 portion = Func.get_objects_list_on_disk(os.path.dirname(file),
-                                                        file_name.split(mask)[0], only_files=True)
+                                                        mask=file_name.split(current_mask)[0], only_files=True)
                 result.extend(portion)
         return result
 
-    def __read_create_date(self, backup_manifest: str):
-        with open(backup_manifest) as json_file:
-            data = json.load(json_file)
-            for p in data['Files']:
-                if p['Path'] == 'backup_label':
-                    return p['Last-Modified']
+    def __read_create_date(self, backup: str):
+
+        try:
+            if '__backup_manifest' in backup:
+                with open(backup) as json_file:
+                    data = json.load(json_file)
+                    for p in data['Files']:
+                        if p['Path'] == 'backup_label':
+                            date_str = p['Last-Modified']
+                            break
+                json_file.close()
+            else:
+                if backup.endswith("tar.gz"):
+                    tar = tarfile.open(backup, "r:gz")
+                elif backup.endswith("tar"):
+                    tar = tarfile.open(backup, "r:")
+
+                ex_file = tar.extractfile('backup_label')
+
+                while True:
+                    # считываем строку
+                    line = ex_file.readline()
+                    # прерываем цикл, если строка пустая
+                    if not line:
+                        break
+                    # выводим строку
+                    text = str(line.strip())
+                    if 'START TIME:' in text:
+                        date_str = text.split('START TIME:')[1]
+                        date_str = date_str.replace('\'', '')
+                        date_str = date_str.replace(' ', '', 1)
+                        break
+
+                    # закрываем файл
+                ex_file.close()
+        except:
+            date_str = None
+
+        if date_str is None:
+            return None
+        tz = date_str.split(' ')[2]
+        tzinfos = ''
+        for item in self.whois_timezone_info.items():
+            if item[0] == tz:
+                tzinfos = {item[0]:item[1]}
+                break
+
+        result = parser.parse(date_str, tzinfos=tzinfos)
+
+        return result
 
     def __delete_local_empty_bck_dirs(self):
         arr = [self.args.path_to_full_backup_local(), self.args.local_path_to_wal_files()]
@@ -1045,7 +1353,7 @@ class Manager:
 
         storage_time = 1565100
         expire_date = now - datetime.timedelta(seconds=storage_time)
-        self.__cleaner.clean_local(expire_date)
+        self.__cleaner._clean_local(expire_date)
         self.__connector._clean_cloud()
 
     def clean_backups(self, write_to_log_file=True, raise_exception=False):
